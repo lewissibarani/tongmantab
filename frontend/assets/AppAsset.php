95b2736de6f0acdb03614f68944e5694f0830ef0
unknown <Administrator@003221-1-04.BPS.go.id>
2021-03-25 16:05:01 +0700

    Nambahin Folder Theme

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,6 +13,9 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        '//fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic',
+        '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800',
+        '//fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i',
     ];
     public $js = [
     ];
